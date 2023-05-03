d3972b8b5fe81cde51d02f373fae469d2a1ab5db
jeonghun <wjdgns2751@naver.com>
2023-05-04 00:16:53 +0900

    pro 레벨테스트 1단계

---
+++ leveltest/pro_카카오숫자놀이.java
@@ -0,0 +1,33 @@
+package leveltest;
+
+import java.util.*;
+
+public class pro_카카오숫자놀이 {
+
+    public int solution(String s) {
+        int answer = 0;
+        String res = "";
+        String[] num = new String[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        List<String> list = Arrays.asList(num);
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            boolean isNum = Character.isDigit(s.charAt(i));
+            if (isNum) {
+                res += s.charAt(i);
+            } else {
+                sb.append(s.charAt(i));
+                if (list.contains(sb.toString())) {
+                    res += list.indexOf(sb.toString());
+                    sb.setLength(0);
+                }
+            }
+        }
+        answer = Integer.parseInt(res);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ leveltest/pro_평균값.java
@@ -0,0 +1,20 @@
+package leveltest;
+
+public class pro_평균값 {
+    public static double solution(int[] arr) {
+        double answer = 0;
+
+        for (int cnt : arr)
+            answer += cnt;
+
+        answer /= arr.length;
+
+        return answer;
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4 };
+        System.out.println(solution(arr));
+    }
+}
