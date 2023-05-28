b085766d51262354c98ec7561651f3084d9808ba
jeonghun <wjdgns2751@naver.com>
2023-05-28 14:42:49 +0900

    수학은 체육과목입니다.

---
+++ baek/rectangle_triangle/baek_15894.java
@@ -0,0 +1,11 @@
+package baek.rectangle_triangle;
+
+import java.util.Scanner;
+
+public class baek_15894 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextLong() * 4);
+        sc.close();
+    }
+}
