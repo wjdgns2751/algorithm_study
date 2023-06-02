c1f3a6c7dde914e543d390aaf1fa7b749e629631
jeonghun <wjdgns2751@naver.com>
2023-06-02 21:27:25 +0900

    알고리즘의 수행 시간 2

---
+++ baek/time_complexity/baek_24263.java
@@ -0,0 +1,10 @@
+package baek.time_complexity;
+
+import java.io.*;
+
+public class baek_24263 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine() + "\n1");
+    }
+}
