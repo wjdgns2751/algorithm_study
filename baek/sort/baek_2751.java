f8357837dabab5c8642bb3716d653f3282214aeb
jeonghun <wjdgns2751@naver.com>
2023-06-13 22:54:54 +0900

    수 정렬하기2

---
+++ baek/sort/baek_2751.java
@@ -0,0 +1,29 @@
+package baek.sort;
+
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class baek_2751 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++)
+            list.add(Integer.parseInt(br.readLine()));
+
+        Collections.sort(list);
+
+        for (int i = 0; i < list.size(); i++)
+            sb.append(list.get(i)).append('\n');
+
+        System.out.println(sb);
+    }
+}
