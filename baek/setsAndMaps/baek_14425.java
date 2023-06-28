9844f7f3095787d15e4b646882a7e95607b1ff2f
jeonghun <wjdgns2751@naver.com>
2023-06-28 23:01:15 +0900

    문자열 집합

---
+++ baek/setsAndMaps/baek_14425.java
@@ -0,0 +1,25 @@
+package baek.setsAndMaps;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class baek_14425 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            map.put(br.readLine(), 0);
+        }
+        int count = 0;
+        for (int i = 0; i < M; i++) {
+            if (map.containsKey(br.readLine()))
+                count++;
+        }
+        System.out.print(count);
+    }
+}
