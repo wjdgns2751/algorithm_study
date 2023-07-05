fae1583bde109c7622998cc19a7460cd8b433786
jeonghun <wjdgns2751@naver.com>
2023-07-05 23:06:18 +0900

    대칭차집합

--- baek/setsAndMaps/baek_1269.java
+++ baek/setsAndMaps/baek_1269.java
@@ -1,42 +1,34 @@
 package baek.setsAndMaps;
 
 import java.io.*;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class baek_1269 {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
 
-        Set<Integer> setA = new HashSet<>();
-        Set<Integer> setB = new HashSet<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
 
         st = new StringTokenizer(br.readLine(), " ");
 
-        for (int i = 0; i < A; i++)
-            setA.add(Integer.parseInt(st.nextToken()));
-
-        for (int i = 0; i < B; i++)
-            setB.add(Integer.parseInt(st.nextToken()));
-
-        int answer = 0;
-        for (int num : setA) {
-            if (!setB.contains(num)) {
-                answer += 1;
-            }
+        for (int i = 0; i < A; i++) {
+            int number = Integer.parseInt(st.nextToken());
+            map.put(number, 1);
         }
 
-        for (int num : setB) {
-            if (!setA.contains(num)) {
-                answer += 1;
-            }
+        st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < B; i++) {
+            int number = Integer.parseInt(st.nextToken());
+            map.put(number, map.getOrDefault(number, 0) + 1);
         }
 
-        System.out.println(answer);
+        System.out.println(map.values().stream().filter(num -> num == 1).count());
+
     }
 }
